73a4014ac7e05eefdc6978794e7e037d8d3054c2
jcrodriguez445 <juanksr208@gmail.com.com>
2025-12-02 16:30:36 -0500

    correccion conexion supabase

--- app/supabase_client.py
+++ app/supabase_client.py
@@ -1,8 +1,7 @@
 from supabase import create_client
 import os
 
-# Claves de tu Supabase
-SUPABASE_URL = os.getenv("SUPABASE_URL", "https://fdujitwtuecibozsxytk.supabase.co")
-SUPABASE_KEY = os.getenv("SUPABASE_KEY", "sb_secret_dIPA5z6gkzPG7C7O3Pb_RA_Q80ojOOX")
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 
 supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
