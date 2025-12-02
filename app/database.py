6fdb354b3e43e48a0e2d56a384e414e7219ba33b
jcrodriguez445 <juanksr208@gmail.com.com>
2025-12-02 14:41:28 -0500

    mejora para la base de datos conexion a render

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ from sqlmodel import SQLModel, create_engine, Session
 from typing import Generator
 import os
 
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./app/finanzas.db")
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./finanzas.db")
 
 engine = create_engine(
     DATABASE_URL,
