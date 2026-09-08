a71a1a2ed20a3eed0152888a9e79d00e888139eb
Danskers <derodriguez20@ucatolica.edu.co>
2026-09-08 13:47:36 -0500

    Add tests for API and OpenAPI response

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,17 @@
+from fastapi.testclient import TestClient
+from main import app
+
+client = TestClient(app)
+
+
+def test_api_responde():
+    response = client.get("/")
+
+    assert response.status_code == 200
+
+
+def test_openapi():
+    response = client.get("/openapi.json")
+
+    assert response.status_code == 200
 
